6e155032b4ebbad4cf516249360332fb8b9d028c
spricigom <emilispricigo.ifc@gmail.com>
2025-04-22 08:38:54 -0300

    feat: inclusão do relacionamento n para n entre Livro e Autor

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,13 +1,24 @@
 from django.db import models
 
+from .autor import Autor
 from .categoria import Categoria
+from .editora import Editora
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, )
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    categoria= models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
-  
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
+    autor = models.ManyToManyField(
+        Autor, related_name="livros", blank=True 
+    )
+
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
