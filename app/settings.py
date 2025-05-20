640f6c335c2b36adf28c772a986a6a6015b9973c
spricigom <emilispricigo.ifc@gmail.com>
2025-05-20 09:33:38 -0300

    feat: criação uploader

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'rest_framework',
     'core',
+    'uploader'
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -8,6 +10,7 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, UserViewSet
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
@@ -31,6 +34,10 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc',
     ),
+    path(
+        "api/media/", include(uploader_router.urls)
+        ), 
     # API
     path('api/', include(router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
