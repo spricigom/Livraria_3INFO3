0a810d82c2ac77383c4523c68297f3357a4dfa1e
spricigom <emilispricigo.ifc@gmail.com>
2025-07-04 14:12:38 -0300

    feat: criação do endpoint para a listagem básica de compras

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,14 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, CompraViewSet, EditoraViewSet, LivroViewSet, UserViewSet
 from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
 router.register(r'autores', AutorViewSet, basename='autores')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
+router.register(r'compras', CompraViewSet, basename='compras')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
 router.register(r'livros', LivroViewSet, basename='livros')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,5 +1,6 @@
 from .user import UserViewSet
 from .autor import AutorViewSet
 from .categoria import CategoriaViewSet
+from .compra import CompraViewSet
 from .editora import EditoraViewSet
 from .livro import LivroViewSet
